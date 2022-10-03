c0002516f9008fb74d2e7b9b4fbe7bd9491ec7b2
Usmaan Bajwa <ukbajwa10@gmail.com>
2022-10-03 18:06:46 -0500

    Added game difficulty, replay feature

--- app.py
+++ app.py
@@ -1,74 +1,106 @@
 import random
 from extra.hangman import hangman_graphics, animals_sorted, states_sorted, food_sorted
 
-print("1) Animals")
-print("2) States of the U.S.A.")
-print("3) Food Dishes")
+while True:
+    print("\n1) Animals")
+    print("2) States and Provinces of the U.S.A.")
+    print("3) Food Dishes")
 
-game_word = ""
-topic_choice = input("Select a game topic: ")
+    game_word = ""
+    topic_choice = input("Select a game topic: ")
+    print("\n1) Easy. In Easy mode, shorter words are in the selection pool.")
+    print("2) Normal. In Normal mode, there is no restriction on the word selection.")
+    print("3) Hard. In Hard mode, longer words are in the selection pool.")
+    game_mode = input("Select a game mode: ")
 
-if topic_choice == "1":
-    print("You selected 'Animals'!")
-    game_word = random.choice(animals_sorted).lower()
-elif topic_choice == "2":
-    print("You selected 'States of the United States of America'!")
-    game_word = random.choice(states_sorted).lower()
-elif topic_choice == "3":
-    print("You selected 'Food Dishes'!")
-    game_word = random.choice(food_sorted).lower()
+    if topic_choice == "1" and game_mode == 1:
+        print("You selected 'Animals' (EASY)!")
+        game_word_index = random.randint(0, 112)
+        game_word = animals_sorted[game_word_index].lower()
+    elif topic_choice == "1" and game_mode == 2:
+        print("You selected 'Animals' (NORMAL)!")
+        game_word = random.choice(animals_sorted).lower()
+    elif topic_choice == "1" and game_mode == 3:
+        print("You selected 'Animals' (HARD)!")
+        game_word_index = random.randint(113, 187)
+        game_word = animals_sorted[game_word_index].lower()
+    elif topic_choice == "2" and game_mode == "1":
+        print("You selected 'States and Provinces of the United States of America' (EASY)!")
+        game_word_index = random.randint(0, 34)
+        game_word = states_sorted[game_word_index].lower()
+    elif topic_choice == "2":
+        print(
+            "You selected 'States and Provinces of the United States of America' (NORMAL)!")
+        game_word = random.choice(states_sorted).lower()
+    elif topic_choice == "2" and game_mode == "3":
+        print("You selected 'States and Provinces of the United States of America' (HARD)!")
+        game_word_index = random.randint(35, 54)
+        game_word = states_sorted[game_word_index].lower()
+    elif topic_choice == "3":
+        print("You selected 'Food Dishes'!")
+        game_word = random.choice(food_sorted).lower()
 
-# Reference hangman.py for num of lives; Num of lives corresponds to each graphic, 6 is the highest and player dies at 0
-number_of_lives = 6
-game_word_list = list(game_word)
-number_of_blanks = []
-guessed_letters = []
+    # Reference hangman.py for num of lives; Num of lives corresponds to each graphic, 6 is the highest and player dies at 0
+    number_of_lives = 6
+    game_word_list = list(game_word)
+    number_of_blanks = []
+    guessed_letters = []
 
-# Populate the blanks list with blanks equal to the length of the word
-for x in game_word:
-    number_of_blanks.append("_")
+    # Populate the blanks list with blanks equal to the length of the word
+    for x in game_word:
+        number_of_blanks.append("_")
 
-# If there are spaces in the word, auto-insert those into the solution so the player doesnt have to guess spaces
-if " " in game_word_list:
-    space_indices = [i for i, x in enumerate(
-        game_word_list) if x == " "]
-    for space_index in space_indices:
-        number_of_blanks.pop(space_index)
-        number_of_blanks.insert(space_index, " ")
+    # If there are spaces in the word, auto-insert those into the solution so the player doesnt have to guess spaces
+    if " " in game_word_list:
+        space_indices = [i for i, x in enumerate(
+            game_word_list) if x == " "]
+        for space_index in space_indices:
+            number_of_blanks.pop(space_index)
+            number_of_blanks.insert(space_index, " ")
 
-print(hangman_graphics[number_of_lives])
-print(*number_of_blanks, sep=" ")
-print(game_word)
+    print(hangman_graphics[number_of_lives])
+    print(*number_of_blanks, sep=" ")
+    # print(game_word) <-- Un-comment for testing purposes
 
-while number_of_lives > 0 and "_" in number_of_blanks:
-    letter_guess = input("Guess a letter: ").lower()
-    if letter_guess in game_word_list:
-        guessed_letters.append(letter_guess)
-        print("Good guess!")
-        letter_indices = [i for i, x in enumerate(
-            game_word_list) if x == letter_guess]
-        for letter_index in letter_indices:
-            number_of_blanks.pop(letter_index)
-            number_of_blanks.insert(letter_index, letter_guess)
-            game_word_list.pop(letter_index)
-            game_word_list.insert(letter_index, "_")
-        print(hangman_graphics[number_of_lives])
-        print(*number_of_blanks, sep=" ")
-        print("")
-        print("Letters guessed so far: ", *guessed_letters, sep=' ')
-        print("")
-    elif letter_guess not in game_word_list:
-        guessed_letters.append(letter_guess)
-        print("Bad guess, you lost a life!")
-        number_of_lives -= 1
-        print(hangman_graphics[number_of_lives])
-        print("")
-        print("Letters guessed so far: ", *guessed_letters, sep=' ')
-        print("")
-        print(*number_of_blanks, sep=" ")
+    while number_of_lives > 0 and "_" in number_of_blanks:
+        letter_guess = input("Guess a letter: ").lower()
+        if letter_guess in game_word_list:
+            guessed_letters.append(letter_guess)
+            print("Good guess!")
+            letter_indices = [i for i, x in enumerate(
+                game_word_list) if x == letter_guess]
+            for letter_index in letter_indices:
+                number_of_blanks.pop(letter_index)
+                number_of_blanks.insert(letter_index, letter_guess)
+                game_word_list.pop(letter_index)
+                game_word_list.insert(letter_index, "_")
+            print(hangman_graphics[number_of_lives])
+            print(*number_of_blanks, sep=" ")
+            print("\nLetters guessed so far: ", *guessed_letters, sep=' ')
 
+        elif letter_guess in guessed_letters:
+            print(
+                "\nYou already tried that, please pay attention to the 'letters guessed so far'!")
 
-if number_of_lives == 0:
-    print("You lose!")
-else:
-    print("You win!")
+        elif letter_guess not in game_word_list:
+            guessed_letters.append(letter_guess)
+            print("Bad guess, you lost a life!")
+            number_of_lives -= 1
+            print(hangman_graphics[number_of_lives])
+            print("\nLetters guessed so far: ", *guessed_letters, sep=' ')
+            print(*number_of_blanks, sep=" ")
+
+    if number_of_lives == 0:
+        print(f"You lose!\nThe word was {game_word.capitalize()}")
+        keep_playing = input("Would you like to play again? 'Yes or No' ")
+        if keep_playing.lower() == "no":
+            exit()
+        else:
+            pass
+    else:
+        print("You win!")
+        keep_playing = input("Would you like to play again? 'Yes or No' ")
+        if keep_playing.lower() == "no":
+            exit()
+        else:
+            pass
--- extra/hangman.py
+++ extra/hangman.py
@@ -1,3 +1,4 @@
+import random
 hangman_graphics = ['''
   +---+
   |   |
@@ -58,8 +59,8 @@ hangman_graphics.reverse()
 # the longer section will be from x - 100. using random choice and range, randomly pick a value from either the shorter section for ez mode, longer section for hard mode
 # for normal mode just ignore this logic
 
-animals = ["Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cassowary", "Cat", "Caterpillar", "Cattle", "Chamois", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Chough", "Clam", "Cobra", "Cockroach", "Cod", "Cormorant", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Curlew", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dotterel", "Dove", "Dragonfly", "Duck", "Dugong", "Dunlin", "Eagle", "Echidna", "Eel", "Eland", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gaur", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Gnu", "Goat", "Goldfinch", "Goldfish", "Goose", "Gorilla", "Goshawk", "Grasshopper", "Grouse", "Guanaco", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish", "Kangaroo", "Kingfisher", "Koala", "Kookabura", "Kouprey", "Kudu", "Lapwing",
-           "Lark", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Loris", "Louse", "Lyrebird", "Magpie", "Mallard", "Manatee", "Mandrill", "Mantis", "Marten", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Quelea", "Quetzal", "Rabbit", "Raccoon", "Rail", "Ram", "Rat", "Raven", "Reddeer", "Redpanda", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "SandDollar", "Sandpiper", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Shrew", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Spoonbill", "Squid", "Squirrel", "Starling", "Stingray", "Stinkbug", "Stork", "Swallow", "Swan", "Tapir", "Tarsier", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker", "Worm", "Wren", "Yak", "Zebra"]
+animals = ["Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cat", "Caterpillar", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Clam", "Cobra", "Cockroach", "Cod", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dove", "Dragonfly", "Duck", "Eagle", "Eel", "Eland", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Goat", "Goldfish", "Goose", "Gorilla", "Grasshopper", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish", "Kangaroo", "Koala", "Kookabura", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Magpie",
+           "Mallard", "Manatee", "Mandrill", "Mantis", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Rabbit", "Raccoon", "Rail", "Ram", "Rat", "Raven", "Redpanda", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Squid", "Squirrel", "Starling", "Stingray", "Stork", "Swallow", "Swan", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodpecker", "Worm", "Yak", "Zebra"]
 
 usa_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi",
               "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
@@ -76,4 +77,15 @@ def Sorting(list):
 animals_sorted = Sorting(animals)
 states_sorted = Sorting(usa_states)
 food_sorted = Sorting(food)
-print(food_sorted)
+
+print(len(animals))
+# print(len(usa_states)) = 55
+# print(len(food)) = 6
+
+game_word_index_easy = random.randint(0, 112)
+
+game_word_index_hard = random.randint(113, 187)
+
+print(animals_sorted[game_word_index_easy])
+print(animals_sorted[game_word_index_hard])
+print(animals_sorted[160])
