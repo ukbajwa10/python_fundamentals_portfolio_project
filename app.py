34b8077dc5700a7f230d02e7f2b5d0aec43944c5
Usmaan Bajwa <ukbajwa10@gmail.com>
2022-10-04 15:43:14 -0500

    Cleaned up code, removed fluff

--- app.py
+++ app.py
@@ -1,52 +1,58 @@
 import random
 from extra.hangman import hangman_graphics, animals_sorted, states_sorted, food_sorted
 
+
 while True:
     print("\n1) Animals")
     print("2) States and Provinces of the U.S.A.")
     print("3) Food Dishes")
 
-    game_word = ""
     topic_choice = input("Select a game topic(1, 2, or 3): ")
     print("\n1) Easy. In Easy mode, shorter words are in the selection pool.")
     print("2) Normal. In Normal mode, there is no restriction on the word selection.")
     print("3) Hard. In Hard mode, longer words are in the selection pool.")
-    game_mode = input("Select a game mode(1, 2, or 3): ")
+    game_difficulty = input("Select a game mode(1, 2, or 3): ")
 
-    if topic_choice == "1" and game_mode == "1":
-        print("You selected 'Animals' (EASY)!")
-        game_word_index = random.randint(0, 112)
-        game_word = animals_sorted[game_word_index].lower()
-    elif topic_choice == "1" and game_mode == "2":
-        print("You selected 'Animals' (NORMAL)!")
-        game_word = random.choice(animals_sorted).lower()
-    elif topic_choice == "1" and game_mode == "3":
-        print("You selected 'Animals' (HARD)!")
-        game_word_index = random.randint(113, 187)
-        game_word = animals_sorted[game_word_index].lower()
-    elif topic_choice == "2" and game_mode == "1":
-        print("You selected 'States and Provinces of the United States of America' (EASY)!")
-        game_word_index = random.randint(0, 34)
-        game_word = states_sorted[game_word_index].lower()
-    elif topic_choice == "2" and game_mode == "2":
-        print(
-            "You selected 'States and Provinces of the United States of America' (NORMAL)!")
-        game_word = random.choice(states_sorted).lower()
-    elif topic_choice == "2" and game_mode == "3":
-        print("You selected 'States and Provinces of the United States of America' (HARD)!")
-        game_word_index = random.randint(35, 54)
-        game_word = states_sorted[game_word_index].lower()
-    elif topic_choice == "3" and game_mode == "1":
-        print("You selected 'Food Dishes' (EASY)!")
-        game_word_index = random.randint(0, 14)
-        game_word = food_sorted[game_word_index].lower()
-    elif topic_choice == "3" and game_mode == "2":
-        print("You selected 'Food Dishes' (NORMAL)!")
-        game_word = random.choice(food_sorted).lower()
-    elif topic_choice == "3" and game_mode == "3":
-        print("You selected 'Food Dishes' (HARD)!")
-        game_word_index = random.randint(15, 26)
-        game_word = food_sorted[game_word_index].lower()
+    def game_mode():
+        global game_word
+        if topic_choice == "1" and game_difficulty == "1":
+            print("You selected 'Animals' (EASY)!")
+            game_word_index = random.randint(0, 112)
+            game_word = animals_sorted[game_word_index].lower()
+        elif topic_choice == "1" and game_difficulty == "2":
+            print("You selected 'Animals' (NORMAL)!")
+            game_word = random.choice(animals_sorted).lower()
+        elif topic_choice == "1" and game_difficulty == "3":
+            print("You selected 'Animals' (HARD)!")
+            game_word_index = random.randint(113, 187)
+            game_word = animals_sorted[game_word_index].lower()
+        elif topic_choice == "2" and game_difficulty == "1":
+            print(
+                "You selected 'States and Provinces of the United States of America' (EASY)!")
+            game_word_index = random.randint(0, 34)
+            game_word = states_sorted[game_word_index].lower()
+        elif topic_choice == "2" and game_difficulty == "2":
+            print(
+                "You selected 'States and Provinces of the United States of America' (NORMAL)!")
+            game_word = random.choice(states_sorted).lower()
+        elif topic_choice == "2" and game_difficulty == "3":
+            print(
+                "You selected 'States and Provinces of the United States of America' (HARD)!")
+            game_word_index = random.randint(35, 54)
+            game_word = states_sorted[game_word_index].lower()
+        elif topic_choice == "3" and game_difficulty == "1":
+            print("You selected 'Food Dishes' (EASY)!")
+            game_word_index = random.randint(0, 14)
+            game_word = food_sorted[game_word_index].lower()
+        elif topic_choice == "3" and game_difficulty == "2":
+            print("You selected 'Food Dishes' (NORMAL)!")
+            game_word = random.choice(food_sorted).lower()
+        elif topic_choice == "3" and game_difficulty == "3":
+            print("You selected 'Food Dishes' (HARD)!")
+            game_word_index = random.randint(15, 26)
+            game_word = food_sorted[game_word_index].lower()
+
+    game_mode()
 
     # Reference hangman.py for num of lives; Num of lives corresponds to each graphic, 6 is the highest and player dies at 0
     number_of_lives = 6
--- extra/hangman.py
+++ extra/hangman.py
@@ -52,6 +52,7 @@ hangman_graphics = ['''
 
 hangman_graphics.reverse()
 
+# Word Lists
 animals = ["Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cat", "Caterpillar", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Clam", "Cobra", "Cockroach", "Cod", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dove", "Dragonfly", "Duck", "Eagle", "Eel", "Eland", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Goat", "Goldfish", "Goose", "Gorilla", "Grasshopper", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish", "Kangaroo", "Koala", "Kookabura", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Magpie",
            "Mallard", "Manatee", "Mandrill", "Mantis", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Rabbit", "Raccoon", "Rail", "Ram", "Rat", "Raven", "Redpanda", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Squid", "Squirrel", "Starling", "Stingray", "Stork", "Swallow", "Swan", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodpecker", "Worm", "Yak", "Zebra"]
 
@@ -60,6 +61,8 @@ usa_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "Cal
 
 food = ["Cheeseburgers", "Pasta", "Pizza",
         "French Fries", "Chicken Wings", "Spaghetti", "Chicken Nuggets", "Steak", "Apple Pie", "Jambalaya", "Macaroni and Cheese", "Grilled Cheese Sandwich", "Waffles", "Pancakes", "Hot Dogs", "Barbecue", "Cheesecake", "Cake", "Cookies", "Brownies", "Noodles", "Kung Pao Chicken", "Fajitas", "Corn", "Omelette", "Chicken Tikka Masala"]
+
+# Sort the lists by word length in order to implement a game difficulty
 
 
 def Sorting(list):
@@ -70,15 +73,3 @@ def Sorting(list):
 animals_sorted = Sorting(animals)
 states_sorted = Sorting(usa_states)
 food_sorted = Sorting(food)
-
-# print(len(food))
-# print(len(usa_states)) = 55
-# print(len(food)) = 6
-
-# game_word_index_easy = random.randint(0, 14)
-
-# game_word_index_hard = random.randint(15, 26)
-
-# print(food_sorted[game_word_index_easy])
-# print(food_sorted[game_word_index_hard])
-# print(food_sorted[16])
